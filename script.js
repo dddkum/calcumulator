439fc7b523cafe5eb2c530ec150ea5de145bae6f
Dmitry Kumachev <dkumachev18@gmail.com>
2023-04-03 11:44:27 +0400

    script.js upd

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 let screen = document.getElementById("result")
 let button = document.getElementsByClassName("btn")
 console.log(button)
+
+for (let i = 0; i < button.length; i++) {
+    button[i].addEventListener("click", function(event){
+        buttonText = event.target.value
+        if(buttonText == "C"){
+            screen.value = ""
+        } else if(buttonText == "*"){
+            screen.value += "*"
+        } else if (buttonText == "="){
+            let equalValue = eval(screen.value)
+            screen.value = equalValue
+        } else {
+            screen.value += buttonText
+        }
+    })
+}
